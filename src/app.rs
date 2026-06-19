0e2078193ec6e4d3fb388788c9d6972b10283d21
Kalilamodow <kalilamodow@gmail.com>
2026-06-18 20:07:09 -0500

    fix player list not updating
    huge brainfart with using channels initially but now the player list is
    just a mutex

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use crate::ranks::{Rank, RankAPI};
 use crate::rl_stats_api::{self, Platform, PlayerData, RLEvent};
 use eframe::egui;
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 use std::time::Duration;
 
@@ -40,9 +40,8 @@ impl Rank {
 }
 
 pub struct RankDisplayApp {
-    players_receiver: mpsc::Receiver<Vec<PlayerData>>,
     error_receiver: mpsc::Receiver<String>,
-    players: Option<Vec<PlayerData>>,
+    players: Arc<Mutex<Option<Vec<PlayerData>>>>,
     player_ranks: RankAPI,
     current_error: Option<String>,
 }
@@ -68,12 +67,12 @@ fn schedule_overlay_flyover(ctx: egui::Context) {
 
 impl RankDisplayApp {
     pub fn new(ctx: &eframe::CreationContext) -> Self {
-        let (player_tx, player_rx) = mpsc::channel();
         let (errors_tx, errors_rx) = mpsc::channel();
 
+        let players = Arc::new(Mutex::new(None));
+
         let app = RankDisplayApp {
-            players: None,
-            players_receiver: player_rx,
+            players: Arc::clone(&players),
             error_receiver: errors_rx,
             player_ranks: RankAPI::new(ctx.egui_ctx.clone(), errors_tx.clone()),
             current_error: None,
@@ -82,9 +81,11 @@ impl RankDisplayApp {
         let ctx = ctx.egui_ctx.clone();
         thread::spawn(move || {
             let result = rl_stats_api::connect_to_stats_api(|event| match event {
-                RLEvent::SetPlayerList(players) => {
-                    player_tx.send(players).unwrap();
-                    ctx.request_repaint();
+                RLEvent::SetPlayerList(new_players) => {
+                    if let Ok(mut players) = players.lock() {
+                        *players = Some(new_players);
+                        ctx.request_repaint();
+                    }
                 }
                 RLEvent::MatchStart => schedule_overlay_flyover(ctx.clone()),
             });
@@ -98,87 +99,90 @@ impl RankDisplayApp {
     }
 
     fn render_main_content(&mut self, ui: &mut egui::Ui) {
-        if let Some(players) = &self.players {
-            egui::Grid::new("player list")
-                .num_columns(5)
-                .spacing([12.0, 12.0])
-                .striped(true)
-                .min_row_height(32.0)
-                .show(ui, |ui| {
-                    ui.label(bold_text("Name"));
-                    ui.label(bold_text("Platform"));
-                    ui.label(bold_text("1s"));
-                    ui.label(bold_text("2s"));
-                    ui.label(bold_text("3s"));
-                    ui.end_row();
+        let Ok(lock) = self.players.lock() else {
+            return;
+        };
 
-                    for player in players {
-                        ui.label(&player.name);
-                        ui.label(player.platform.to_string());
-
-                        if player.platform == Platform::Bot {
-                            ui.label("-");
-                            ui.label("-");
-                            ui.label("-");
-                        } else if let Some(player_skills) = self.player_ranks.get(&player) {
-                            let modes = [
-                                &player_skills.ranked_1s,
-                                &player_skills.ranked_2s,
-                                &player_skills.ranked_3s,
-                            ];
-
-                            for skill in modes {
-                                if let Some(skill) = skill {
-                                    let response =
-                                        ui.image(skill.rank.to_image()).on_hover_text(format!(
-                                            "{}\nMMR: {}{}",
-                                            skill.rank.as_str(),
-                                            skill.mmr,
-                                            skill
-                                                .rank_is_estimate
-                                                .then(|| "\nRank estimate based on MMR")
-                                                .unwrap_or_default()
-                                        ));
-
-                                    if skill.rank_is_estimate {
-                                        // warning badge
-                                        let rect = response.rect;
-                                        let badge_center = egui::Pos2::new(
-                                            rect.right() - 4.0,
-                                            rect.bottom() - 4.0,
-                                        );
-
-                                        ui.painter().circle_filled(
-                                            badge_center,
-                                            4.0,
-                                            egui::Color32::RED,
-                                        );
-
-                                        ui.painter().text(
-                                            badge_center,
-                                            egui::Align2::CENTER_CENTER,
-                                            "!",
-                                            egui::FontId::proportional(8.0),
-                                            egui::Color32::WHITE,
-                                        );
-                                    }
-                                } else {
-                                    ui.image(Rank::Unranked.to_image())
-                                        .on_hover_text("No data for gamemode");
-                                }
-                            }
-                        } else {
-                            ui.spinner();
-                            ui.spinner();
-                            ui.spinner();
-                        }
-                        ui.end_row();
-                    }
-                });
-        } else {
+        let Some(players) = &*lock else {
             ui.label("Waiting for game...");
             ui.spinner();
-        }
+            return;
+        };
+
+        egui::Grid::new("player list")
+            .num_columns(5)
+            .spacing([12.0, 12.0])
+            .striped(true)
+            .min_row_height(32.0)
+            .show(ui, |ui| {
+                ui.label(bold_text("Name"));
+                ui.label(bold_text("Platform"));
+                ui.label(bold_text("1s"));
+                ui.label(bold_text("2s"));
+                ui.label(bold_text("3s"));
+                ui.end_row();
+
+                for player in players {
+                    ui.label(&player.name);
+                    ui.label(player.platform.to_string());
+
+                    if player.platform == Platform::Bot {
+                        ui.label("-");
+                        ui.label("-");
+                        ui.label("-");
+                    } else if let Some(player_skills) = self.player_ranks.get(&player) {
+                        let modes = [
+                            &player_skills.ranked_1s,
+                            &player_skills.ranked_2s,
+                            &player_skills.ranked_3s,
+                        ];
+
+                        for skill in modes {
+                            if let Some(skill) = skill {
+                                let response =
+                                    ui.image(skill.rank.to_image()).on_hover_text(format!(
+                                        "{}\nMMR: {}{}",
+                                        skill.rank.as_str(),
+                                        skill.mmr,
+                                        skill
+                                            .rank_is_estimate
+                                            .then(|| "\nRank estimate based on MMR")
+                                            .unwrap_or_default()
+                                    ));
+
+                                if skill.rank_is_estimate {
+                                    // warning badge
+                                    let rect = response.rect;
+                                    let badge_center =
+                                        egui::Pos2::new(rect.right() - 4.0, rect.bottom() - 4.0);
+
+                                    ui.painter().circle_filled(
+                                        badge_center,
+                                        4.0,
+                                        egui::Color32::RED,
+                                    );
+
+                                    ui.painter().text(
+                                        badge_center,
+                                        egui::Align2::CENTER_CENTER,
+                                        "!",
+                                        egui::FontId::proportional(8.0),
+                                        egui::Color32::WHITE,
+                                    );
+                                }
+                            } else {
+                                ui.image(Rank::Unranked.to_image())
+                                    .on_hover_text("No data for gamemode");
+                            }
+                        }
+                    } else {
+                        ui.spinner();
+                        ui.spinner();
+                        ui.spinner();
+                    }
+                    ui.end_row();
+                }
+            });
     }
 }
 
@@ -186,9 +190,6 @@ impl eframe::App for RankDisplayApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         if let Ok(new_error) = self.error_receiver.try_recv() {
             self.current_error = Some(new_error);
-        }
-        if let Ok(new_players) = self.players_receiver.try_recv() {
-            self.players = Some(new_players);
         }
 
         egui::CentralPanel::default().show_inside(ui, |ui| {
