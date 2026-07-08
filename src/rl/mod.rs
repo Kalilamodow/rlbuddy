2b462f320579d4f7a4ad5b76fdfd767eb07d89b2
Kalilamodow <kalilamodow@gmail.com>
2026-07-07 19:42:09 -0500

    rl module models module

--- src/rl/mod.rs
+++ src/rl/mod.rs
@@ -1,3 +1,6 @@
+mod models;
+pub use models::*;
+
 mod names;
 mod skills;
 mod stats_api;
@@ -5,13 +8,3 @@ mod stats_api;
 pub use names::*;
 pub use skills::*;
 pub use stats_api::*;
-
-mod arena;
-mod match_state;
-mod playlist;
-mod rank;
-
-pub use arena::*;
-pub use match_state::*;
-pub use playlist::*;
-pub use rank::*;
---
+++ src/rl/models/mod.rs
@@ -0,0 +1,9 @@
+mod arena;
+mod match_state;
+mod playlist;
+mod rank;
+
+pub use arena::*;
+pub use match_state::*;
+pub use playlist::*;
+pub use rank::*;
