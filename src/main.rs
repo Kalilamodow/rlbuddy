976b54bbf3ecf362304323f1e32dc50e6a45fc24
Kalilamodow <kalilamodow@gmail.com>
2026-07-12 21:37:27 -0500

    increase default window height

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use eframe::egui;
 fn main() -> eframe::Result {
     let gui_options = eframe::NativeOptions {
         renderer: eframe::Renderer::Glow,
-        viewport: egui::ViewportBuilder::default().with_inner_size([350.0, 400.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([350.0, 600.0]),
         ..Default::default()
     };
 
